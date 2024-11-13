f31ad47e0e201bcb3f114905e84047f550ea9aca
Sree Reddy Menon <srikanthreddy29@hotmail.com>
2024-11-12 21:26:40 -0600

    Update Constants.kt
    API PLACE HOLDER

--- app/src/main/java/com/assessment/android/utils/Constants.kt
+++ app/src/main/java/com/assessment/android/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.assessment.android.utils
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "0eb876eed28f9f21f60e0733b53d292f"
+    const val API_KEY = "PLACE_HOLDER_API_KEY"
     const val UNITS: String = "metric"
     const val DEGREE_VALUE = "°"
 }
